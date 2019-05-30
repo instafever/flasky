abdef717f6651a04291630b892b4d56eb74a497d
Instafever <495140158@qq.com>
2019-05-30 19:05:32 +0800

    remove shell context processor function

--- run.py
+++ run.py
@@ -2,18 +2,11 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from app import create_app, db
-from app.models import Model
 
 app = create_app()
 manager = Manager(app)
 migrate = Migrate(app, db)
 manager.add_command('db', MigrateCommand)
-
-
-@app.shell_context_processor
-def make_shell_context():
-    return dict(db=db, Model=Model)
-
 
 if __name__ == '__main__':
     manager.run()
